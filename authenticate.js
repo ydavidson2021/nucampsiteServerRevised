5583d0aebf54a599cd800e2fe37be96c3cdf6971
Yenny D <ydavidson2021@gmail.com>
2020-11-03 12:41:13 -0600

    Passport Facebook

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ const User = require('./models/user'); //user model - user schema has access to 
 const JwtStrategy = require('passport-jwt').Strategy; //jwtStrategy constructor from the passowrt jwt library
 const ExtractJwt = require('passport-jwt').ExtractJwt; //helper methods
 const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const FacebookTokenStrategy = require('passport-facebook-token');
 
 const config = require('./config.js');
 const { NotExtended } = require('http-errors');
@@ -15,17 +16,6 @@ passport.deserializeUser(User.deserializeUser()); //session based authentication
 exports.getToken = function(user) {
     return jwt.sign(user, config.secretKey, {expiresIn: 3600});
 };
-
-exports.verifyAdmin = function(req, res, next) { 
-    if (req.user.admin) {
-        return next();
-    } else {
-        const err = new Error('You are not authorized to perform this operation');
-        err.status = 403;
-        return next(err);
-    }
-};
-
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
@@ -49,7 +39,45 @@ exports.jwtPassport = passport.use(
     )
 );
 
-//Task 1 
-
-
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = function(req, res, next) { 
+    if (req.user.admin) {
+        return next();
+    } else {
+        const err = new Error('You are not authorized to perform this operation');
+        err.status = 403;
+        return next(err);
+    }
+};
+//similar to jwt Strategy
+exports.facebookPassport = passport.use(
+    new FacebookTokenStrategy(
+        {
+            clientID: config.facebook.clientId,
+            clientSecret: config.facebook.clientSecret
+        }, 
+        (accessToken, refreshToken, profile, done) => { //verify callback function done callback same as jwt strategy
+            User.findOne({facebookId: profile.id}, (err, user) => { //check if we already have an account 
+                if (err) {
+                    return done(err, false);
+                }
+                if (!err && user) {
+                    return done(null, user); // if no error but user already exists
+                } else { // no error and no user. 
+                    user = new User({ username: profile.displayName }); //create new user database
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.save((err, user) => { //save to mongodb database
+                        if (err) {
+                            return done(err, false);
+                        } else {
+                            return done(null, user);
+                        }
+                    });
+                }
+            });
+        }
+    )
+);
