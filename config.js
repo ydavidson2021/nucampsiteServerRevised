d15c4935ff2f862e872f26310379ec10ee134666
Yenny D <ydavidson2021@gmail.com>
2020-11-03 13:46:22 -0600

    edit

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321', // sign jsonwebtoken 
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite', //url for mongoDB server 
-    'facebook': {
-        clientId: '383983509623557',
-        clientSecret: 'dead0a3398b4f39841b62006124d4cf6'
-    }
-}
